909bb2aac6d3ee40b566a4c872f0868c1eb98f23
RoPoWisc <43710823+RoPoWisc@users.noreply.github.com>
2020-03-23 18:49:39 -0500

    rCommit-1

--- App.js
+++ App.js
@@ -2,29 +2,15 @@ import { AppLoading } from 'expo';
 import { Asset } from 'expo-asset';
 import * as Font from 'expo-font';
 import React, { useState } from 'react';
+import LoginScreen from './screens/LoginScreen';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import AppNavigator from './navigation/AppNavigator';
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
-
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
+export default class App extends React.Component {
+  render() {
+    return <LoginScreen />;
   }
 }
 
